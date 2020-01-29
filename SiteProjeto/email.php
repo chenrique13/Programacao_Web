0c45d40ca27fe6f2f6749c2845e0e0e50da76f21
Carlos Henrique do Nascimento Pereira <c.henrique1309@gmail.com>
2020-01-29 01:55:31 -0300

    Atualização

--- SiteProjeto/email.php
+++ SiteProjeto/email.php
@@ -1,32 +1,31 @@
 <?php
 $mensagem = "
-
-	nome: $_POST[nome]
-	email: $_POST[email]
-	areas: $_POST[areas]
-	git: $_POST[git]
+	Nome: $_POST[nome]
+	E-mail: $_POST[email]
+	Áreas: $_POST[areas]
+	GitHub: $_POST[git]
 	linkedin: $_POST[linkedin]
-	impCurriculo: $_POST[impCurriculo]
-	impFoto: $_POST[impFoto]";
+	Curriculo: $_POST[impCurriculo]
+	Foto: $_POST[impFoto]";
 
 $dest = 'c.henrique1309@gmail.com';
 
-$enviar = mail( $dest, 'Banco SPW', $mensagem);
+$enviar = mail( $dest, 'Banco SPW', $mensagem );
 
 
-if($enviar){
+if ( $enviar ) {
 	?>
-<script>
-	alert( 'Enviado com Sucesso!');
-	history.go(-1);
-</script>
-<?php
-}else{
-?>
-<script>
-	alert( 'Erro');
-	history.go(-1);
-</script>
-<?php
+	<script>
+		alert( 'Enviado com Sucesso!' );
+		history.go( -1 );
+	</script>
+	<?php
+} else {
+	?>
+	<script>
+		alert( 'Erro' );
+		history.go( -1 );
+	</script>
+	<?php
 }
 ?>
